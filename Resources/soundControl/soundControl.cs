b26a3655227731dfffd78d1c0acc5f26293787b8
FrAzenV2 <54308705+FrAzenV2@users.noreply.github.com>
2019-10-12 11:24:40 +0300

    music added

---
+++ Resources/soundControl/soundControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class soundControl : MonoBehaviour
+{
+    public GameObject childSound;
+    public GameObject childMusic;
+    public AudioClip[] soundClip;
+    AudioSource sound;
+    AudioSource music;
+    void Start()
+    {
+        sound = childSound.GetComponent<AudioSource>();
+        music = childMusic.GetComponent<AudioSource>();
+        music.Play();
+        soundClip = new AudioClip[9];
+    }
+
+    public void MusicPlay(){
+        music.Play();
+    }
+
+    public void MusicStop(){
+        music.Stop();
+    }
+
+    public void SoundPlay(int audioID){
+        sound.clip = soundClip[audioID];
+        sound.Play();
+    }
+}
