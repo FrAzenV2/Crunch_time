865e81229b122d1c3d5a0d305c327f8ea1627bad
FrAzenV2 <54308705+FrAzenV2@users.noreply.github.com>
2019-10-12 09:01:53 +0300

    ещ тще дщщыу

---
+++ Resources/Scripts/Card_Rotater.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Card_Rotater : MonoBehaviour
+{
+
+    public GameObject backSide;
+    public GameObject frontSide;
+
+
+    Sequence mySequence; 
+
+    public void TurnCard(int which_seq)
+    {
+
+        if (DOTween.PlayingTweens().Count == 0)
+        if (which_seq == 1)
+        {
+            mySequence = DOTween.Sequence();
+            //first seq
+            // Add a movement tween at the beginning
+            mySequence.Append(frontSide.transform.DORotate(new Vector3(0, 90, 0), 0.5f));
+            // Add a rotation tween as soon as the previous one is finished
+            mySequence.Append(backSide.transform.DORotate(new Vector3(0, 90, 0), 0.5f, RotateMode.WorldAxisAdd));
+        }
+        else if (which_seq == 2)
+        {
+            mySequence = DOTween.Sequence();
+
+            mySequence.Append(backSide.transform.DORotate(new Vector3(0, -90, 0), 0.5f));
+            mySequence.Append(frontSide.transform.DORotate(new Vector3(0, 0, 0), 0.5f));
+            mySequence.Append(frontSide.transform.DORotate(new Vector3(0, 90, 0), 0.5f));
+            // Add a rotation tween as soon as the previous one is finished
+            mySequence.Append(backSide.transform.DORotate(new Vector3(0, 90, 0), 0.5f, RotateMode.WorldAxisAdd));
+
+        }
+        else if( which_seq == 0)
+        {
+
+        }
+    }
+}
---
+++ Resources/Scripts/InkManager.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Ink.Runtime;
+
+public class InkManager : MonoBehaviour
+{
+    // UI Prefabs
+    [SerializeField]
+    private Text variant1;
+    [SerializeField]
+    private Text variant2;
+    
+    public Text storyText;
+
+    public Card_Rotater card_Rotater;
+
+    [SerializeField]
+    private TextAsset inkJSONAsset;
+
+    public Story story;
+
+    [SerializeField]
+    private Canvas canvas;
+
+    public bool storyEnded = false;
+    private bool firstTime = true;
+
+    public Choice choice0;
+    public Choice choice1;
+
+    CharacterManager cm;
+    Game_Manager gm;
+
+    void Start()
+    {
+        //cm = GetComponent<CharacterManager>();
+        //gm = GetComponent<Game_Manager>(); 
+        // Remove the default message
+        //RemoveChildren();
+        //Debug.Log("!@@!@!");
+        StartStory();
+        //RefreshView();
+    }
+
+    void StartStory()
+    {
+        storyEnded = false;
+        firstTime = true;
+        story = new Story(inkJSONAsset.text);
+        //Debug.Log("!@@!@!");
+        RefreshView();
+    }
+    void RefreshView()
+    {
+
+
+        // Read all the content until we can't continue any more
+        while (story.canContinue)
+        {
+            // Continue gets the next line of the story
+            string text = story.Continue();
+            // This removes any white space from the text.
+            text = text.Trim();
+            // Display the text on screen!
+            storyText.text = text;
+        }
+        if (firstTime)
+        {
+            firstTime = !firstTime;
+            card_Rotater.TurnCard(1);
+        }
+
+        Debug.Log("!@!@!@!");
+        // Display all the choices, if there are any!
+        if (story.currentChoices.Count > 0)
+        {
+            //Debug.Log("!@@@!");
+            Debug.Log(story.currentChoices.Count);
+            choice0 = story.currentChoices[0];
+            choice1 = story.currentChoices[1];
+
+            variant1.text = choice0.text.Trim();
+            variant2.text = choice1.text.Trim();
+            
+            //ChoiseSelecter();
+        }
+        // If we've read all the content and there's no choices, the story is finished!
+        else
+        {
+            storyEnded = true;
+            variant1.text = "End of the day";
+            variant2.text = "End of the day";
+        }
+        //Debug.Log("!@@!!@!");
+        
+    }
+
+
+    public void ChooseLeft()
+    {
+        if (storyEnded)
+        {
+            StartStory();
+        }
+        else
+        {
+            card_Rotater.TurnCard(2);
+            //story.ChooseChoiceIndex(choice0.index);
+            StartCoroutine(Delay(0));
+            //RefreshView();
+        }
+    }
+
+    public void ChooseRight()
+    {
+        if (storyEnded)
+        {
+
+            StartStory();
+        }
+        else
+        {
+            card_Rotater.TurnCard(2);
+            StartCoroutine(Delay(1));
+            //story.ChooseChoiceIndex(choice1.index);
+            
+        }
+    }
+
+    void ChoiseSelecter()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Swipe_detector.directionX == -1)
+            {
+                story.ChooseChoiceIndex(choice0.index);
+            }
+            else if (Swipe_detector.directionX == 1)
+            {
+                story.ChooseChoiceIndex(choice1.index);
+            }
+            //Debug.Log("!@!!!@@!");
+            Swipe_detector.Reset_SwipeX();
+            RefreshView();
+        }
+    }
+
+    IEnumerator Delay(int i)
+    {
+        yield return new WaitForSeconds(0.3f);
+        if (i == 1)
+        {
+            story.ChooseChoiceIndex(choice1.index);
+            yield return new WaitForSeconds(0.2f);
+            RefreshView();
+        }
+        else if (i == 0)
+        {
+            story.ChooseChoiceIndex(choice0.index);
+            yield return new WaitForSeconds(0.2f);
+            RefreshView();
+        }
+    }
+    
+}
